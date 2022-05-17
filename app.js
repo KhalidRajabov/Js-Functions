bf5a522ccbc5420e083986f43e229b501d3b9002
dbBurzum <khalidsr@code.edu.az>
2022-05-18 01:28:44 +0400

    done

---
+++ app.js
@@ -0,0 +1,34 @@
+//2) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
+
+// function multiplier(array) {
+//   let first = 0;
+
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 == 0) {
+//       first += Math.pow(array[i], 2);
+//       console.log(array[i]+" * "+array[i]+" = "+ Math.pow(array[i], 2));
+//     }
+//   }
+//   return first;
+// }
+
+// console.log("final: " + multiplier([1,,3,4,5,6,7,8,9,10]));
+
+
+
+
+
+
+
+
+//3) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+
+// function isItDivided(params){
+//     for (let i = 0; i < params.length; i++) {
+//         if (params[i]%3==0&&params[i]%7==0) {
+//             console.log( params[i] + ". Becuase: " + (params[i]+ " / " + "3" + " = " + (params[i] / 3)) + "       &       " + (params[i]+ " / " + "7" + " = " + (params[i] / 7)))        
+//         }
+//     }
+// }
+// isItDivided([3,4,5,7,21,42,49,84, 105, 140])
